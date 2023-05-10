b491b08b20e73eab99f8bee4bb53a47babaf905d
Waseel Fawad <waseelfawad@gmail.com>
2023-05-11 03:35:38 +0500

    feat(Tailwind,Font): tailwind css integrated and gilroy font added

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
+      },
+    },
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      red: '#F21E1E',
+      white: '#fcfcfc',
+      black: '#272d2f',
+      gray: '#b9b9b9',
+    },
+    fontFamily: {
+      gilroy: ['Gilroy', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
