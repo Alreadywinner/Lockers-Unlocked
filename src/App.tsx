05431d75a4e5fa23ae45aed46e65589979891ece
Waseel Fawad <waseelfawad@gmail.com>
2023-05-11 03:41:06 +0500

    feat(Routing): react routing in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Input } from '@components';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { Button } from 'components';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-5xl font-bold underline">Hello world!</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => {
-            setCount((val) => val + 1);
-          }}
-          className="buttonStyle"
-        >
-          count is {count}
-        </button>
-        <Input />
-        <Button />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Button text="testing" className="bg-red" />} />
+      {/* <Route path="/about" element={<AboutPage />} /> */}
+    </Routes>
   );
 }
 
