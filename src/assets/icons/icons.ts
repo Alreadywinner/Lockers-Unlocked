6e205372789521dcfacb2d82fee8cff5cbe289f8
Waseel Fawad <waseelfawad@gmail.com>
2023-05-11 03:37:36 +0500

    feat(SVG): svg configuration added for project

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,6 @@
+import { ReactComponent as UserIcon } from './user.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as FacebookIcon } from './facebook.svg';
+import { ReactComponent as TwitterIcon } from './twitter.svg';
+
+export { UserIcon, InstagramIcon, FacebookIcon, TwitterIcon };
