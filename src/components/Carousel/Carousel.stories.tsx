440fbadd3bd80f705379e36eae70dcf2e7cdccfa
Waseel Fawad <waseelfawad@gmail.com>
2023-06-01 03:05:39 +0500

    feat(Carousel): carousel removed now only logo will displayed on screen

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -27,30 +27,7 @@ type Story = StoryObj<typeof Carousel>;
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 
 export const Template: Story = {
-  render: (args) => <Carousel {...args} />,
+  render: () => <Carousel />,
 };
 
-Template.args = {
-  slides: [
-    {
-      src: 'https://plus.unsplash.com/premium_photo-1681406994502-bb673c265877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      key: 1,
-    },
-    {
-      src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/files/Screen_Shot_2022-05-19_at_8.11.14_AM_1944x.png?v=1652962312',
-      key: 2,
-    },
-    {
-      src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/files/exp_1944x.png?v=1628726052',
-      key: 3,
-    },
-    {
-      src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/files/Website_3_5e3e742a-0d1a-4f78-be90-fea8890f3d7a_1944x.jpg?v=1680872229',
-      key: 4,
-    },
-    {
-      src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/files/Website_5_1944x.png?v=1683254520',
-      key: 5,
-    },
-  ],
-};
+Template.args = {};
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,91 +1,13 @@
-import { NextIcon, PreviousIcon } from '@Icon';
-import React, { useState } from 'react';
-import { Button } from '@components';
-import { CarouselType } from './types';
+import { MainLogoIcon } from '@Icon';
+import React from 'react';
 
-export default function Carousel({ slides }: CarouselType) {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const handlePrevClick = () => {
-    setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
-  };
-
-  const handleNextClick = () => {
-    setCurrentSlide((currentSlide + 1) % slides.length);
-  };
-
-  const handleSlideClick = (index: number) => {
-    setCurrentSlide(index);
-  };
+export default function Carousel() {
   return (
-    <div
-      id="default-carousel"
-      className="relative w-full"
-      data-carousel="slide"
-    >
+    <div className="w-full overflow-hidden">
       {/* Carousel wrapper */}
-      <div className="relative overflow-hidden">
-        {/* Slides */}
-        {slides &&
-          slides?.map((slide, index) => (
-            <div
-              key={slide.key}
-              className={`${
-                index === currentSlide ? 'block' : 'hidden'
-              } transition duration-500 ease-in-out transform hover:scale-110`}
-              data-carousel-item
-            >
-              <img
-                src={slide.src}
-                className="w-full h-full cursor-pointer object-cover"
-                alt="..."
-              />
-            </div>
-          ))}
+      <div className="flex items-center justify-center cursor-pointer transition duration-500 ease-in-out transform hover:scale-110 overflow-hidden">
+        <MainLogoIcon className="md:h-[40rem] h-[35rem]" />
       </div>
-      {/* Slider indicators */}
-      <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        {slides &&
-          slides.map((element, index) => (
-            <button
-              key={element.key}
-              type="button"
-              className={`w-3 h-3 rounded-full ${
-                index === currentSlide
-                  ? 'bg-white'
-                  : 'bg-gray-300 dark:bg-gray-600'
-              }`}
-              aria-current={index === currentSlide}
-              aria-label={`Slide ${index + 1}`}
-              data-carousel-slide-to={index}
-              onClick={() => handleSlideClick(index)}
-            />
-          ))}
-      </div>
-      {/* Slider controls */}
-      <Button
-        type="button"
-        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
-        onClick={handlePrevClick}
-      >
-        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <PreviousIcon color="black" />
-          <span className="sr-only">Previous</span>
-        </span>
-      </Button>
-      <Button
-        type="button"
-        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
-        onClick={handleNextClick}
-      >
-        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <NextIcon color="black" />
-
-          <span className="sr-only">Next</span>
-        </span>
-      </Button>
     </div>
   );
 }
