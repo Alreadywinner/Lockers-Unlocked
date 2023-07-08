dfaf25dc018fc13e4ca9a7d9f9b311d369993037
Waseel Fawad <waseelfawad@gmail.com>
2023-07-08 15:41:29 +0500

    style(App): all cateories page minor style updates for responsive screens

--- src/containers/D1SportsPage/D1SportsPage.tsx
+++ src/containers/D1SportsPage/D1SportsPage.tsx
@@ -1,15 +1,20 @@
 import { Button, Card } from '@components';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function D1SportsPage() {
   return (
-    <div className="font-gilroy mt-8">
-      <Button
-        className="bg-red text-white p-3 rounded float-right hover:bg-red500 mr-3"
-        type="button"
-      >
-        Add a New Item
-      </Button>
+    <div className="font-gilroy mt-8 flex flex-col">
+      <div>
+        <Link to="/add-new">
+          <Button
+            className="bg-red text-white p-3 rounded float-right hover:bg-red500 mr-3"
+            type="button"
+          >
+            Add a New Item
+          </Button>
+        </Link>
+      </div>
       <h1 className="text-4xl font-bold text-center">D1 Sports Items</h1>
       <div className="mt-14 mb-5 flex gap-5 flex-wrap justify-center">
         <Card
