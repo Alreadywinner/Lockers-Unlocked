db9018dc2b7ff2c126bf98100848a5c859f31ce7
Waseel Fawad <waseelfawad@gmail.com>
2023-07-11 04:54:34 +0500

    chore(SignUpForm): sign up form development is in progress

---
+++ src/components/SignUpForm/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SignUpForm() {
+  return <div>index</div>;
+}
+
+export default SignUpForm;
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -16,25 +16,21 @@ export default function HomePage() {
     {
       src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/collections/1200px-Clemson_Tigers_logo.svg_b780f06f-762a-4c15-9ebe-289cdafe6d69_360x.png?v=1617919028',
       text: 'Clemson',
-
       key: 3,
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/collections/1280px-Texas_Longhorns_logo.svg_1e965cb4-d60e-4c84-bb56-d1799a880828_360x.png?v=1617913509',
       text: 'Texas',
-
       key: 4,
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/collections/Michigan_Wolverines_Logo.svg_84f7699e-2b00-4adf-a6aa-6d399d093091_360x.png?v=1679875487',
       text: 'Michigan',
-
       key: 5,
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0414/2111/1457/collections/758px-Syracuse_Orange_logo.svg_f8e474ed-367f-418b-a200-a69b9fd9d426_360x.png?v=1617919614',
       text: 'Syracuse',
-
       key: 6,
     },
   ];
