8da4e086729cf21a993c7ade8ea2ddad114c2ad6
Waseel Fawad <waseelfawad@gmail.com>
2023-07-23 23:28:19 +0500

    feat(FirestoreDB): db integrated in the code with no rules set

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+
+const db = getFirestore(app);
+
+export { app, db };
