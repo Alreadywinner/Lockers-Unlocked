6d32058359329ed232b646b0575033cb5b086500
Waseel Fawad <waseelfawad@gmail.com>
2023-07-24 23:38:39 +0500

    chore(App): lint warnings resolved

--- src/context/localStorageDataContext.ts
+++ src/context/localStorageDataContext.ts
@@ -1,5 +1,5 @@
 // LocalStorageDataContext.tsx
-import React, { createContext, useContext } from 'react';
+import { createContext, useContext } from 'react';
 
 export type LocalStorageDataType = {
   email: string;
