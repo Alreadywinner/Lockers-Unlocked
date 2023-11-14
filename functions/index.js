0c7a8f10a2fa14967151fa42bad450cded2e74f2
Waseel Fawad <waseelfawad@gmail.com>
2023-11-15 02:39:31 +0500

    feat(Stripe): stripe functions are now ready to be deployed

--- functions/index.js
+++ functions/index.js
@@ -8,37 +8,30 @@ const stripe = new Stripe(
   'sk_test_51O7xbUDdwhg4yy2RHesvc3ViQTlcEeV3RUtJ2xjAR69vLaDSHZU1JfvCeL7kyctyonHYOopKoBjOU4tV0c2HZ29a00VuXHiN9l',
 );
 
-// const db = admin.firestore();
-
 exports.createPaymentLink = functions.https.onRequest(async (req, res) => {
   try {
     // Extract data from the request
-    const { productId, amount, currency, description } = req.body;
+    const { amount, description, productName, productImage } = req.body;
 
-    // Fetch product details from Firestore using productId
-    // const productSnapshot = await db
-    //   .collection('products')
-    //   .doc(productId)
-    //   .get();
-    // const productData = productSnapshot.data();
+    const product = await stripe.products.create({
+      name: productName,
+      description,
+      images: [productImage],
+    });
 
-    // if (!productData) {
-    //   throw new Error('Product not found');
-    // }
+    const price = await stripe.prices.create({
+      unit_amount: Math.round(Number(amount) * 100), // The amount in the smallest currency unit (e.g., cents).
+      currency: 'usd',
+      product: product.id, // The ID of the product you created.
+    });
 
-    // Create a payment link
     const paymentLink = await stripe.paymentLinks.create({
       line_items: [
         {
-          price: '100', // Replace with the actual Stripe Price ID
+          price: price.id,
           quantity: 1,
         },
       ],
-      currency: currency || 'usd',
-      description: description || 'Payment description',
-      //   client_reference_id: productId,
-      //   success_url: 'https://your-website.com/success',
-      //   cancel_url: 'https://your-website.com/cancel',
     });
 
     res.json({ url: paymentLink.url });
