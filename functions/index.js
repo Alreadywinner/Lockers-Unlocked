8af4f36a09fa0de11479d674cc91b91763fa4353
Waseel Fawad <waseelfawad@gmail.com>
2023-11-25 15:09:59 +0500

    chore(Stripe): added logic that wont be used in all scenarios

--- functions/index.js
+++ functions/index.js
@@ -13,18 +13,54 @@ exports.createPaymentLink = functions.https.onRequest(async (req, res) => {
     // Extract data from the request
     const { amount, description, productName, productImage } = req.body;
 
-    const product = await stripe.products.create({
-      name: productName,
-      description,
-      images: [productImage],
+    let product = await stripe.products.search({
+      query: `name~"${productName}" AND description~"${description}"`,
     });
+    if (product.data.length === 0) {
+      // Create a new product if it doesn't exist
+      product = await stripe.products.create({
+        attributes: ['name'],
+        name: productName,
+        description,
+        images: [productImage],
+      });
+    } else {
+      // Use the existing product
+      product = product.data[0];
+    }
 
-    const price = await stripe.prices.create({
-      unit_amount: Math.round(Number(amount) * 100), // The amount in the smallest currency unit (e.g., cents).
+    // Check if a price with the same unit amount exists
+    let price = await stripe.prices.list({
+      product: product.id,
+      unit_amount: Math.round(Number(amount) * 100),
       currency: 'usd',
-      product: product.id, // The ID of the product you created.
+      limit: 1,
     });
 
+    if (price.data.length === 0) {
+      // Create a new price if it doesn't exist
+      price = await stripe.prices.create({
+        unit_amount: Math.round(Number(amount) * 100),
+        currency: 'usd',
+        product: product.id,
+      });
+    } else {
+      // Use the existing price
+      price = price.data[0];
+      console.log('Using the existing price');
+    }
+
+    // Check if a payment link with the same product and price exists
+    // let paymentLink = await stripe.paymentLinks.list({
+    //   line_items: [
+    //     {
+    //       price: price.id,
+    //       quantity: 1,
+    //     },
+    //   ],
+    //   limit: 1,
+    // });
+    // let paymentLinks = await stripe.paymentLinks.list({});
     const paymentLink = await stripe.paymentLinks.create({
       line_items: [
         {
@@ -33,8 +69,22 @@ exports.createPaymentLink = functions.https.onRequest(async (req, res) => {
         },
       ],
     });
+    // if (paymentLink.data.length === 0) {
+    //   // Create a new payment link if it doesn't exist
+    //   paymentLink = await stripe.paymentLinks.create({
+    //     line_items: [
+    //       {
+    //         price: price.id,
+    //         quantity: 1,
+    //       },
+    //     ],
+    //   });
+    // } else {
+    //   // Use the existing payment link
+    //   paymentLink = paymentLink.data[0];
+    // }
 
-    res.json({ url: paymentLink.url });
+    res.json({ url: paymentLink });
   } catch (error) {
     console.error('Error creating payment link:', error);
     res.status(500).send('Internal Server Error');
