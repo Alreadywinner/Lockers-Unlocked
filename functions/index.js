17b57a2a2863cd567c6b0d6cd31dfe2d996d6243
Waseel Fawad <waseelfawad@gmail.com>
2023-12-10 23:25:54 +0500

    feat(Backend): email template and firebase function is ready with cors and nodemailer

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const Stripe = require('stripe');
+const nodemailer = require('nodemailer');
+const EmailTemplate = require('./EmailTemplate.js');
+const cors = require('cors')({ origin: true });
 
 admin.initializeApp();
 
@@ -8,85 +11,75 @@ const stripe = new Stripe(
   'sk_test_51O7xbUDdwhg4yy2RHesvc3ViQTlcEeV3RUtJ2xjAR69vLaDSHZU1JfvCeL7kyctyonHYOopKoBjOU4tV0c2HZ29a00VuXHiN9l',
 );
 
-exports.createPaymentLink = functions.https.onRequest(async (req, res) => {
-  try {
-    // Extract data from the request
-    const { amount, description, productName, productImage } = req.body;
+const corsOptions = {
+  // Allow origins based on your requirements
+  origin: [
+    'https://lockers-unlocked.web.app',
+    'https://lockersunlocked.com',
+    'https://www.lockersunlocked.com',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-    let product = await stripe.products.search({
-      query: `name~"${productName}" AND description~"${description}"`,
-    });
-    if (product.data.length === 0) {
-      // Create a new product if it doesn't exist
-      product = await stripe.products.create({
-        attributes: ['name'],
+exports.createPaymentLink = functions.https.onRequest((req, res) =>
+  cors(req, res, async () => {
+    try {
+      // Extract data from the request
+      const {
+        amount,
+        description,
+        productName,
+        productImage,
+        buyerName,
+        buyerEmail,
+      } = req.body;
+
+      const product = await stripe.products.create({
         name: productName,
         description,
         images: [productImage],
       });
-    } else {
-      // Use the existing product
-      product = product.data[0];
-    }
 
-    // Check if a price with the same unit amount exists
-    let price = await stripe.prices.list({
-      product: product.id,
-      unit_amount: Math.round(Number(amount) * 100),
-      currency: 'usd',
-      limit: 1,
-    });
-
-    if (price.data.length === 0) {
-      // Create a new price if it doesn't exist
-      price = await stripe.prices.create({
-        unit_amount: Math.round(Number(amount) * 100),
+      const price = await stripe.prices.create({
+        unit_amount: Math.round(Number(amount) * 100), // The amount in the smallest currency unit (e.g., cents).
         currency: 'usd',
-        product: product.id,
+        product: product.id, // The ID of the product you created.
       });
-    } else {
-      // Use the existing price
-      price = price.data[0];
-      console.log('Using the existing price');
+
+      const paymentLink = await stripe.paymentLinks.create({
+        line_items: [
+          {
+            price: price.id,
+            quantity: 1,
+          },
+        ],
+      });
+
+      if (paymentLink && paymentLink.url !== '') {
+        const paymentLinkUrl = paymentLink.url;
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+            user: 'lockersunlocked@gmail.com',
+            pass: 'ofiuseiueurrlxza',
+          },
+        });
+        let info = await transporter.sendMail({
+          from: '"Locker Unlocked" <lockersunlocked@gmail.com>', // sender address
+          to: `${buyerEmail}`, // list of receivers
+          subject: 'Buy your Product', // Subject line
+          text: 'Congratulations! Now you can Buy your product', // plain text body
+          html: EmailTemplate(buyerName, buyerEmail, paymentLinkUrl), // html body
+        });
+        res.json({ msg: `Message sent successfully ${info.messageId}` });
+      }
+    } catch (error) {
+      console.error('Error creating payment link:', error);
+      res.status(500).send('Internal Server Error');
     }
-
-    // Check if a payment link with the same product and price exists
-    // let paymentLink = await stripe.paymentLinks.list({
-    //   line_items: [
-    //     {
-    //       price: price.id,
-    //       quantity: 1,
-    //     },
-    //   ],
-    //   limit: 1,
-    // });
-    // let paymentLinks = await stripe.paymentLinks.list({});
-    const paymentLink = await stripe.paymentLinks.create({
-      line_items: [
-        {
-          price: price.id,
-          quantity: 1,
-        },
-      ],
-    });
-    // if (paymentLink.data.length === 0) {
-    //   // Create a new payment link if it doesn't exist
-    //   paymentLink = await stripe.paymentLinks.create({
-    //     line_items: [
-    //       {
-    //         price: price.id,
-    //         quantity: 1,
-    //       },
-    //     ],
-    //   });
-    // } else {
-    //   // Use the existing payment link
-    //   paymentLink = paymentLink.data[0];
-    // }
-
-    res.json({ url: paymentLink });
-  } catch (error) {
-    console.error('Error creating payment link:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+  }),
+);
